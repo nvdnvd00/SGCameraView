ca66f583ce4120bf92fa27a56ccf53d1c0eeaf46
Duc Nguyen <sg@SGs-MacBook-Pro.local>
2020-05-27 12:02:05 +0700

    1.0.14

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { requireNativeComponent } from "react-native";
 
-class CameraView extends React.Component {
-  onRecordingEnd = (e) => {
-    const { data } = e.nativeEvent;
-    console.log({ data });
-    this.props.onRecordingEnd(data);
-  };
-  render() {
-    return (
-      <View style={{ width, height }}>
-        <SgCameraView {...this.props} onRecordingEnd={this.onRecordingEnd} />
-      </View>
-    );
-  }
-}
+const SgCameraView = requireNativeComponent("SgCameraView", null);
 
-CameraView.propTypes = {
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  beat: PropTypes.string.isRequired,
-  lyric: PropTypes.array,
-  onRecordingEnd: PropTypes.func.isRequired,
-};
-
-var SgCameraView = requireNativeComponent("SgCameraView", CameraView);
-
-module.exports = SgCameraView;
+export default SgCameraView;
