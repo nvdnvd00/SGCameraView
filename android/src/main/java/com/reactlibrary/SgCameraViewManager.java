6628844bd0cb873522873c63b9f05bc09f38faaa
Duc Nguyen <duc.nguyen@saigonvalley.com>
2021-02-05 00:13:24 +0700

    .

--- android/src/main/java/com/reactlibrary/SgCameraViewManager.java
+++ android/src/main/java/com/reactlibrary/SgCameraViewManager.java
@@ -1,15 +1,19 @@
 package com.reactlibrary;
 
+import android.graphics.Color;
 import android.view.View;
 
 import androidx.appcompat.widget.AppCompatCheckBox;
 
+import com.facebook.react.uimanager.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 
 public class SgCameraViewManager extends SimpleViewManager<View> {
 
     public static final String REACT_CLASS = "SgCameraView";
+    private ThemedReactContext mContext;
+    private View view;
 
     @Override
     public String getName() {
@@ -19,8 +23,15 @@ public class SgCameraViewManager extends SimpleViewManager<View> {
     @Override
     public View createViewInstance(ThemedReactContext c) {
         // TODO: Implement some actually useful functionality
-        AppCompatCheckBox cb = new AppCompatCheckBox(c);
-        cb.setChecked(true);
-        return cb;
+//        AppCompatCheckBox cb = new AppCompatCheckBox(c);
+//        cb.setChecked(true);
+//        return cb;
+        mContext = c;
+        view = new View(c);
+        view.setBackgroundColor(Color.BLUE);
+        return new View(c);
     }
+
+    @ReactProp(name = "beat")
+    public void setBeat(View view,String prop){}
 }
