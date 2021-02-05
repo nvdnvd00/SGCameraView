93c8eb769ddb05bae132d82250f0e5dc3ac33759
Duc Nguyen <nvdnvd00@gmail.com>
2021-02-05 23:01:43 +0700

    .

--- android/src/main/java/com/reactlibrary/SgCameraViewManager.java
+++ android/src/main/java/com/reactlibrary/SgCameraViewManager.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import java.lang.String;
 
+
 import androidx.appcompat.widget.AppCompatCheckBox;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -15,9 +16,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.facebook.react.views.image.ReactImageManager;
 import com.facebook.react.views.image.ReactImageView;
 
@@ -30,10 +34,27 @@ public class SgCameraViewManager extends SimpleViewManager<ReactImageView> {
         mCallerContext = reactContext;
     }
 
+    private RCTEventEmitter mEventEmitter;
+
     public static String mBeat;
     public static ReadableMap mBeatDetail;
     public static ReadableMap mStyle;
     public static ReadableArray mLyrics;
+
+    public enum Events {
+        EVENT_RECORDING_END("onRecordingEnd");
+        private final String mName;
+
+        Events(final String name) {
+            mName = name;
+        }
+
+        @Override
+        public String toString() {
+            return mName;
+        }
+    }
+
 
     @Override
     public String getName() {
@@ -42,11 +63,19 @@ public class SgCameraViewManager extends SimpleViewManager<ReactImageView> {
 
     @Override
     public ReactImageView createViewInstance(ThemedReactContext context) {
-//        AppCompatCheckBox cb = new AppCompatCheckBox(context);
-//        cb.setChecked(true);
-//        return cb;
-        return new ReactImageView(context, Fresco.newDraweeControllerBuilder(), null, mCallerContext);
+        ReactImageView view = new ReactImageView(context, Fresco.newDraweeControllerBuilder(), null, mCallerContext)
+        mEventEmitter = context.getJSModule(RCTEventEmitter.class);
+//        onRecordingEnd(view, "");
+        return view;
     }
+
+
+    public void onRecordingEnd(ReactImageView v, String mergedUrl) {
+        WritableMap event = Arguments.createMap();
+        event.putString("mergedUrl", mergedUrl);
+        mEventEmitter.receiveEvent(v.getId(), Events.EVENT_RECORDING_END.toString(), event);
+    }
+
 
     @ReactProp(name = "beatDetail")
     public void setBeatDetail(ReactImageView view, @NonNull ReadableMap beatDetail) {
